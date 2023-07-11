b0f7350886bcca0622019bc58c509ccf9df93c76
Ujwal Parajuli <ujwal.aerialarts@gmail.com>
2023-07-11 22:10:57 +0545

    Modifies configuration test code

--- test_configurations.py
+++ test_configurations.py
@@ -4,14 +4,24 @@ import logging
 
 def load_configurations():
     with open("configurations/config.json") as f:
-        return json.load(f)
+        configurations = json.load(f)
+        # loop dictionary and store in list of tuples
+        list_tuples = []
+        for config in configurations:
+            tuple_config = (config["config_id"], config["version"], config["input_file"])
+            list_tuples.append(tuple_config)
 
-@pytest.mark.parametrize("config", load_configurations())
-def test_configuration(config):
-    logging.info("Testing configuration: {}".format(config["config_id"]))
-    logging.debug("Testing configuration: {}".format(config["config_id"]))
-    config_id = config["config_id"]
-    version = config["version"]
-    input_file = config["input_file"]
+        return list_tuples
+
+
+
+
+@pytest.mark.parametrize("config_id, version, input_file", load_configurations())
+def test_configuration(config_id, version, input_file, caplog):
+    # logging.info("Testing configuration: {}".format(config["config_id"]))
+    # logging.debug("Testing configuration: {}".format(config["config_id"]))
+    # config_id = config["config_id"]
+    # version = config["version"]
+    # input_file = config["input_file"]
 
     assert config_id == "config_1"
